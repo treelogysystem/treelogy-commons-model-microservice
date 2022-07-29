0c448a7e03cc254c226e3a5432266c0fc9380458
VFELICE <cezar.felipe@pearson.com>
2022-07-29 17:43:08 -0300

    create endpoint UserControler

--- src/main/java/treelogy/br/templateback/models/UserModel.java
+++ src/main/java/treelogy/br/templateback/models/UserModel.java
@@ -1,2 +1,51 @@
-package treelogy.br.templateback.models;public class UserModel {
+package treelogy.br.templateback.models;
+
+import org.springframework.lang.NonNull;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name="tb_user")
+public class UserModel {
+
+    @Id
+    private Long id;
+
+    @NonNull
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @NonNull
+    public String getName() {
+        return name;
+    }
+
+    public void setName(@NonNull String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserModel)) return false;
+        UserModel userModel = (UserModel) o;
+        return Objects.equals(getId(), userModel.getId()) && getName().equals(userModel.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
+    }
+
+
 }
--- src/main/java/treelogy/br/templateback/repositories/UserRepository.java
+++ src/main/java/treelogy/br/templateback/repositories/UserRepository.java
@@ -1,2 +1,13 @@
-package treelogy.br.templateback.repositories;public class UserRepository {
+package treelogy.br.templateback.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import treelogy.br.templateback.models.UserModel;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+    @Query("SELECT u FROM UserModel u WHERE u.id = ?1")
+    UserModel getUserById(Long id);
 }
