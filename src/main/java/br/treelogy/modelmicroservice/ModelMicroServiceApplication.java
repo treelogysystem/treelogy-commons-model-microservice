5d4c0a6d4c3200c059d21bd7fc68c9ce41e2eff4
CezarFelipe <cezarfelipedasilva@gmail.com>
2022-10-05 21:49:09 -0300

    rls-0.0.1

--- src/main/java/br/treelogy/modelmicroservice/ModelMicroServiceApplication.java
+++ src/main/java/br/treelogy/modelmicroservice/ModelMicroServiceApplication.java
@@ -3,8 +3,9 @@ package br.treelogy.modelmicroservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
+@EnableEurekaClient
 @EnableCaching
 @SpringBootApplication
 public class ModelMicroServiceApplication {
